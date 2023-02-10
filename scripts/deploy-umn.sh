9a49cf474a973bded423d37761ba52fb12b52ee6
John Harwell <john.r.harwell@gmail.com>
2023-02-10 15:58:08 -0600

    Add script to make copying to UMN personal webpage trivially easy

---
+++ scripts/deploy-umn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf resources/ public
+hugo --environment production --minify --baseURL https://www-users.cse.umn.edu/~harwe006
+
+module load umn
+
+chmod -R 755 public/ # Not set by default on UMN machines apparently
+
+# Delete all files on the UMN machine under ~/.www that don't
+# currently exist here--they are a result of updates in hugo.
+rsync -avh public/ -e ssh harwe006@$kh01:.www/ --delete
